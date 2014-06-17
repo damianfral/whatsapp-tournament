3e0d7abdbd627b16f8d29de464d67bae1c56782a
damianfral <huevofritopamojarpan@gmail.com>
2014-06-17 20:27:26 +0200

    First commit

---
+++ src/Data/WhatsApp.hs
@@ -0,0 +1,85 @@
+-- # LANGUAGE OverloadedRecordFields #
+module Data.WhatsApp where
+
+import Control.Applicative
+import Control.Arrow
+import Control.Monad
+import Data.Function
+import Data.List
+import Data.Maybe
+import Data.Monoid
+import Text.Parsec hiding ((<|>), many)
+
+type Parser = Parsec String ()
+data WhatsAppMessage = WhatsAppMessage {date :: Date, from :: String, message :: String} deriving (Eq, Ord, Show)
+
+parseWhatsAppChat :: String -> Either ParseError [WhatsAppMessage]
+parseWhatsAppChat = runParser whatsAppChatParser () ""
+
+parseWhatsAppChatFromFile :: String -> IO (Either ParseError  [WhatsAppMessage])
+parseWhatsAppChatFromFile = readFile >=> return . parseWhatsAppChat
+
+whatsAppChatParser :: Parser [WhatsAppMessage]
+whatsAppChatParser = many whatsAppMessageParser
+
+whatsAppMessageParser :: Parser WhatsAppMessage
+whatsAppMessageParser = do
+	dat <- whatsAppDateParser
+	from <- whatsAppNameParser
+	message <- manyTill anyChar newline
+	return $ WhatsAppMessage dat from message
+
+whatsAppNameParser :: Parser String
+whatsAppNameParser = manyTill anyChar $ string ": "
+
+numberParser :: Parser Int
+numberParser = many1 digit >>= \d ->return (read d :: Int)
+
+data Date = Date {year :: Int, month :: Int, day :: Int, hour :: Int, minute :: Int}
+	deriving (Eq, Ord, Show)
+
+clockParser :: Parser (Int, Int)
+clockParser = do
+	ho <- numberParser
+	string ":"
+	mi <- numberParser
+	return (ho,mi)
+
+monthParser :: Parser Int
+monthParser =      (string "ene." *> pure 1)
+	       <|>     (string "feb." *> pure 2)
+	       <|> try (string "mar." *> pure 3)
+	       <|> try (string "abr." *> pure 4)
+	       <|> try (string "may." *> pure 5)
+	       <|> try (string "jun." *> pure 6)
+	       <|> try (string "jul." *> pure 7)
+	       <|> try (string "ago." *> pure 8)
+	       <|>     (string "sep." *> pure 9)
+	       <|>     (string "oct." *> pure 10)
+	       <|>     (string "nov." *> pure 11)
+	       <|>     (string "dic." *> pure 12)
+
+whatsAppDateParser :: Parser Date
+whatsAppDateParser = try whatsAppDateParser1 <|> try whatsAppDateParser2
+
+whatsAppDateParser1 :: Parser Date
+whatsAppDateParser1 = do
+	d <- numberParser
+	string " de "
+	m <- monthParser
+	string ", "
+	(ho, mi) <- clockParser
+	string " - "
+	return $ Date 2014 m d ho mi
+
+whatsAppDateParser2 :: Parser Date
+whatsAppDateParser2 = do
+	d <- numberParser
+	string "/"
+	m <- numberParser
+	string "/"
+	y <- numberParser
+	string ", "
+	(ho, mi) <- clockParser
+	string " - "
+	return $ Date y m d ho mi
---
+++ src/Main.hs
@@ -0,0 +1,108 @@
+module Main where
+
+import Control.Applicative
+import Control.Arrow
+import Data.Function
+import Data.List
+import Data.Monoid
+import Data.WhatsApp
+import System.Environment
+import Text.Parsec hiding ((<|>), many)
+import Text.Tabular
+import Text.Tabular.AsciiArt
+
+instance Monoid Int where
+    a `mappend` b = a + b
+    mempty        = 0
+
+data Match  = Match {matchdate :: Date, local :: Int, away :: Int} deriving (Eq, Ord, Show)
+data Result = L | X | A deriving (Eq, Ord, Enum, Show)
+
+isAResultTriplet :: (Date,Either a (Int,Int)) -> Bool
+isAResultTriplet (_, Right _) = True
+isAResultTriplet _ = False
+
+processChat :: [WhatsAppMessage] -> [Match]
+processChat (msgs) = map (\(d, Right (l,a)) -> Match d l a) $ filter isAResultTriplet $ zip dates goals'
+	where
+		dates  = map date msgs
+		goals' = map (runParser (resultParser) () "" . message) msgs
+
+
+resultParser :: Parser (Int, Int)
+resultParser =  do
+	spaces
+	l <- digit
+	spaces *> string "-" *>	spaces
+	a <- digit
+	return (read [l] :: Int, read [a] :: Int)
+
+result  :: Match -> Result
+result  (Match _ l a) | l > a     = L
+                      | l < a     = A
+                      | otherwise = X
+
+clamp :: (Ord a) => a -> a -> a -> a
+clamp mi ma x | x < mi    = mi
+              | x > ma    = ma
+              | otherwise = x
+
+winner :: [Result] -> Result
+winner ms = toEnum (1 + clamp (-1) 1 s)
+	where
+		(i, _, ii) = summarizeResults ms
+		s = ii - i
+
+totalGoals :: [Match] -> (Int, Int)
+totalGoals = foldr1 (<>) . map goals
+
+dot :: (c -> d) -> (a -> b -> c) -> a -> b -> d
+dot = (.).(.)
+
+lf :: (a -> Bool) -> [a] -> Int
+lf = length `dot` filter
+
+summarizeResults :: [Result] -> (Int, Int, Int)
+summarizeResults results = (ones, xs,  twos)
+	where
+		ones = lf (== L) results
+		twos = lf (== A) results
+		xs   = lf (== X) results
+
+sameRound :: Date -> Date -> Bool
+sameRound (Date y1 m1 d1 h1 _) (Date y2 m2 d2 h2 _) = (y1,m1,d1) == (y2,m2,d2) && (abs (h2 `mod` 12) - (h1 `mod` 12)) < 6
+
+summarizeResultsByDay :: [Match] -> (Int, Int, Int)
+summarizeResultsByDay ms = summarizeResults days
+	where
+		resultsGroupedByDate = map (map result) $ groupBy (sameRound `on` matchdate) ms
+		days = map winner resultsGroupedByDate
+
+goals :: Match -> (Int, Int)
+goals = local &&& away
+
+processResult :: [Match] -> Table String String Int
+processResult matches = Table
+	(Group SingleLine [Header "Goles", Header "Wins", Header "Rounds"])
+	(Group SingleLine [Header "Local", Header "Away"])
+ 	[ [golesLocal,    golesAway]
+	, [partidosLocal, partidosAway]
+	, [diasLocal,     diasAway]
+ 	]
+	where
+		(golesLocal, golesAway)          = totalGoals matches
+		(partidosLocal, _, partidosAway) = summarizeResults $ map result matches
+		(diasLocal, _, diasAway)         = summarizeResultsByDay matches
+
+mostCommon :: (Eq a, Ord a) => [a] -> a
+mostCommon = head . maximumBy compare . group
+
+main :: IO ()
+main = do
+	filename:_ <- getArgs
+	matches    <- parseWhatsAppChatFromFile filename
+	putStrLn ""
+	case matches of
+		Right x -> putStrLn $ render id id show . processResult . processChat $ x
+		_       -> print matches
+	putStrLn ""
